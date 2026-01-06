fa80573571a83d6f9d8e868db4341e99fc3c0da7
konstantin <kostyanich02@gmail.com>
2026-01-06 15:29:26 +0500

    changed amount of iterations to 10

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println("Hello and welcome!");
 
         int i = 1;
-        for (; i <= 5; ) {
+        for (; i <= 10; ) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.printf("i = %d\n", i++);
